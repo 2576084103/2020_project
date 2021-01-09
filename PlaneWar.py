2a1e163d5fd9c831e78288375b57fd1b74303bf8
2576084103 <2576084103@qq.com>
2021-01-09 12:49:01 +0800

    PlaneWar

--- PlaneWar.py
+++ PlaneWar.py
@@ -15,7 +15,7 @@ pygame.mixer.init()
 
 bg_size = width, height = 480, 700
 screen = pygame.display.set_mode(bg_size)
-pygame.display.set_caption("PlaneWar-Python")
+pygame.display.set_caption("PlaneWar")
 
 background = pygame.image.load("images/background.png").convert()
 
@@ -122,7 +122,7 @@ def main():
 
     # 统计得分
     score = 0
-    score_font = pygame.font.Font("font/font.ttf", 36)
+    score_font = pygame.font.Font("font/font.ttf", 34)
 
     # 标志是否暂停游戏
     paused = False
